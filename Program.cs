14d8b7f5e64302f6bfca22065cef3ebdfc25feaf
mohamed <mohamedidbellaali@gmail.com>
2024-12-06 13:23:39 +0100

    learning the syntax

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+	static void func1()
+	{
+		Console.WriteLine("abouve");
+	}
+    static void Main()
+    {
+        Console.WriteLine("Hello, world!");
+		func1();
+		func2();
+		
+    }
+
+}
