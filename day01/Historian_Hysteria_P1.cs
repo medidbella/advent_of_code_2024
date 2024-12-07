5e4818b676ed75404129bb36bbba2397d9f2808c
mohamed <mohamedidbellaali@gmail.com>
2024-12-07 20:26:22 +0100

    first chalenge done

---
+++ day01/Historian_Hysteria_P1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+	static int	abs(int number)
+	{
+		if (number < 0)
+			number *= -1;
+		return (number);
+	}
+
+	static void	CreatArrayFromString(string[] Lines, int[] LeftArray, int[] RightArray)
+	{
+		int i = 0;
+		string[] Current;
+		while (i < Lines.Length)
+		{
+			Current = Lines[i].Split("   ");
+			if (Current.Length != 2)
+				break ;
+			LeftArray[i] = Convert.ToInt32(Current[0]);
+			RightArray[i] = Convert.ToInt32(Current[1]);
+			i++;
+		}
+		return;
+	}
+
+    static int Main(string[] Args)
+    {
+		long result = 0;
+		if (!File.Exists("input.txt"))
+			return 1;
+		string Text = File.ReadAllText("input.txt");
+		string[] Lines = Text.Split('\n');
+		int[] LeftArray = new int[Lines.Length - (Lines[Lines.Length - 1].Length == 0 ? 1 : 0)];
+		int[] RightArray = new int[Lines.Length - (Lines[Lines.Length - 1].Length == 0 ? 1 : 0)];
+		CreatArrayFromString(Lines, LeftArray, RightArray);
+		Array.Sort(LeftArray);
+		Array.Sort(RightArray);
+		for(int i = 0; i < RightArray.Length;i++)
+			result += abs(LeftArray[i] - RightArray[i]);
+		Console.WriteLine(result);
+		return 0;
+	}
+}
